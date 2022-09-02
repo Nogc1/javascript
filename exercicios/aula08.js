58fcbaece7849507a7129fa4cee42765f4489942
Nícolas de Oliveira Macedo <106557745+Nogc1@users.noreply.github.com>
2022-09-02 16:57:35 -0300

    Novos exercícios

---
+++ exercicios/aula08.js
@@ -0,0 +1,6 @@
+var vel = 59.5
+console.log(`A velociddade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('Você está acima da velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança!')
---
+++ exercicios/aula09.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log(`Estrangeiro`)
+}
+else {
+    console.log('Brasileiro')
+}
