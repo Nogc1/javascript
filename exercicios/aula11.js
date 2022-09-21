0dd2456ccb402b202b4a2d3efd263e61b7899693
Nícolas de Oliveira Macedo <106557745+Nogc1@users.noreply.github.com>
2022-09-21 15:32:05 -0300

    novo exercícios

---
+++ exercicios/aula11.js
@@ -0,0 +1,8 @@
+var idade = 19
+if (idade < 18){
+    console.log('Não vota')
+} else  if (idade >= 16 || idade > 65){
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
